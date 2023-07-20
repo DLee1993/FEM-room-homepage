5be4bc0a16e6a6aecfdc9872dd4dd9c2602bbbfe
David Lee <Dai180293@gmail.com>
2023-07-21 00:10:10 +0100

    added base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                spartan: ["League Spartan", "sans-serif"],
+            },
+            colors: {
+                white: "hsl(0, 0%, 100%)",
+                black: "hsl(0, 0%, 0%)",
+                gray: "hsl(0, 0%, 63%)",
+                darkGray: "hsl(0, 0%, 27%)",
+            },
+        },
     },
     plugins: [],
 };
